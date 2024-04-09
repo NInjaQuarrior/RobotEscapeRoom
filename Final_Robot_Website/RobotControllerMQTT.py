9a2b005a9e01d41e20544dd0dde421979e8da1ae
IainMcE <102005550+IainMcE@users.noreply.github.com>
2024-04-09 09:16:23 -0400

    trimmed some stuff, added robot controller, need to run it simultaneously with the website

---
+++ Final_Robot_Website/RobotControllerMQTT.py
@@ -0,0 +1,169 @@
+import paho.mqtt.client as mqtt
+import keyboard
+
+
+client = mqtt.Client(client_id="Robot_Controller")
+mqtt_address = '192.168.1.33'
+
+motionTopic = "robot/motion"
+cameraTopic = "robot/util/camera"
+armTopic = "robot/util/arm"
+blckLightTopic = "robot/util/black"
+ledTopic = "robot/util/led"
+
+
+status = "stop"
+def mqtt_setup():
+    client.connect(mqtt_address, 1883)
+    print("connected")
+
+class Commands:
+    def __init__(self):
+        self.status = "stop"
+        self.camStatus = "stop"
+        self.LEDStatus = "off"
+        self.BlackStatus = "off"
+
+com  = Commands()
+
+def sendForward(self):
+    if com.status != "for":
+        print("sending Forward")
+        client.publish(motionTopic,payload="for")
+        com.status = "for"
+
+def sendStop(self):
+    if com.status != "stop":
+        print("sending stop")
+        client.publish(motionTopic,payload="stop")
+        com.status = "stop"
+
+def sendBack(self):
+    if com.status != "back":
+        print("sending back")
+        client.publish(motionTopic,payload="back")
+        com.status = "back"
+
+def sendLeft(self):
+    if com.status != "left":
+        print("sending left")
+        client.publish(motionTopic,payload="left")
+        com.status = "left"
+
+def sendRight(self):
+    if com.status != "right":
+        print("sending right")
+        client.publish(motionTopic,payload="right")
+        com.status = "right"
+
+def sendUp(self):
+    if com.camStatus != "up":
+        print("sending up")
+        client.publish(cameraTopic,payload="up")
+        com.status = "up"
+
+def sendDown(self):
+    if com.camStatus != "down":
+        print("sending down")
+        client.publish(cameraTopic,payload="down")
+        com.status = "down"
+def sendCamStop(self):
+    if com.camStatus != "stop":
+        print("sending stop")
+        client.publish(cameraTopic,payload="stop")
+        com.status = "stop"
+
+def sendArmUp(self):
+    if com.camStatus != "armup":
+        print("sending armup")
+        client.publish(armTopic,payload="armup")
+        com.status = "armup"
+
+def sendArmDown(self):
+    if com.camStatus != "armdown":
+        print("sending armdown")
+        client.publish(armTopic,payload="armdown")
+        com.status = "armdown"
+def sendArmStop(self):
+    if com.camStatus != "armstop":
+        print("sending armstop")
+        client.publish(armTopic,payload="armstop")
+        com.status = "armstop"
+
+def toggleLED(self):
+    if com.LEDStatus == "off":
+        print("sending on")
+        client.publish(ledTopic,payload="on")
+        com.LEDStatus = "on"
+    else:
+        print("sending off")
+        client.publish(ledTopic,payload="off")
+        com.LEDStatus = "off"
+
+def toggleBlack(self):
+    if com.BlackStatus == "off":
+        print("sending on")
+        client.publish(blckLightTopic,payload="on")
+        com.BlackStatus = "on"
+    else:
+        print("sending off")
+        client.publish(blckLightTopic,payload="off")
+        com.BlackStatus = "off"
+
+
+def setUpBinds():
+        #forward
+        keyboard.on_press_key("w",sendForward)
+        keyboard.on_release_key("w",sendStop)
+        keyboard.on_press_key("up",sendForward)
+        keyboard.on_release_key("up",sendStop)
+
+        #backward
+        keyboard.on_press_key("s",sendBack)
+        keyboard.on_release_key("s",sendStop)
+        keyboard.on_press_key("down",sendBack)
+        keyboard.on_release_key("down",sendStop)
+
+        #right
+        keyboard.on_press_key("d",sendRight)
+        keyboard.on_release_key("d",sendStop)
+        keyboard.on_press_key("right",sendRight)
+        keyboard.on_release_key("right",sendStop)
+
+        #left
+        keyboard.on_press_key("a",sendLeft)
+        keyboard.on_release_key("a",sendStop)
+        keyboard.on_press_key("left",sendLeft)
+        keyboard.on_release_key("left",sendStop)
+
+        #camera up
+        keyboard.on_press_key("e",sendUp)
+        keyboard.on_release_key("e",sendCamStop)
+        
+
+        #camera down
+        keyboard.on_press_key("q",sendDown)
+        keyboard.on_release_key("q",sendCamStop)
+
+        #arm up
+        keyboard.on_press_key("r",sendArmUp)
+        keyboard.on_release_key("r",sendArmStop)
+        
+        #arm down
+        keyboard.on_press_key("f",sendArmDown)
+        keyboard.on_release_key("f",sendArmStop)
+
+        #toggle led
+        keyboard.on_press_key("l",toggleLED)
+
+        #toggle blacklight
+        keyboard.on_press_key("b",toggleBlack)
+
+
+
+        
+if __name__ == '__main__':
+    mqtt_setup()
+    #com = Commands()
+    setUpBinds()
+    keyboard.wait()
--- Final_Robot_Website/web_stream.py
+++ Final_Robot_Website/web_stream.py
@@ -1,18 +1,7 @@
-"""
-    File Created By: Liang Lu
-    File Created On: Mar. 31, 2023
-    Description:
-        this files streams video to html
-        aruco generator: https://chev.me/arucogen/
-        https://www.youtube.com/watch?v=v5a7pKSOJd8
-        https://towardsdatascience.com/video-streaming-in-web-browsers-with-opencv-flask-93a38846fe00
-"""
 # Import necessary libraries
 from flask import Flask, render_template, redirect, request, url_for, session, send_file, Response
 from flask_session import Session
 import cv2
-import cv2.aruco as aruco
-import numpy as np
 import os
 import paho.mqtt.client as mqtt
 
@@ -58,110 +47,10 @@ def mqtt_setup() -> mqtt:
 
 # --- MQTT Setup Ends ---
 
-
-def loadAugImages(path):  # loads image into dictionary
-    myList = os.listdir(path)
-    numMarkers = len(myList)
-    # remove unneeded files from myList
-    if '.DS_Store' in myList:
-        myList.remove('.DS_Store')
-    # add keys into library
-    augDics = {}
-    for imgPath in myList:
-        key = int(os.path.splitext(imgPath)[0])
-        imgAug = cv2.imread(f'{path}/{imgPath}')
-        augDics[key] = imgAug
-    return augDics
-
-
-def findArucoMarkers(img, markerSize=6, totalMarker=250, draw=True):
-    # function to detect for aruco
-    # default is set to 6x6, where there is a total of 250 combinations
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarker}')
-    # for raspberry pi (three lines):
-    arucoDict = aruco.Dictionary_get(key)
-    arucoParam = aruco.DetectorParameters_create()
-    bboxs, ids, rejected = aruco.detectMarkers(
-        imgGray, arucoDict, parameters=arucoParam)
-
-    # for testing (four lines); OpenCV 4.7.x:
-    # dictionary = aruco.getPredefinedDictionary(key)
-    # parameters = aruco.DetectorParameters()
-    # detector = aruco.ArucoDetector(dictionary, parameters)
-    # bboxs, ids, rejected = detector.detectMarkers(img)
-
-    # look for aruco of id = 203 and send to broker
-    if ids is not None and scan_enabled:
-        for id in ids:
-            if id == [203]:
-                client.publish("robot/aruco", payload="aruco valid")
-
-    # if draw == true, a box will be drawn around the code if detected
-    if draw:
-        aruco.drawDetectedMarkers(img, bboxs)
-
-    return [bboxs, ids]
-
-
-def augmentAruco(bbox, id, img, imgAug, drawID=True):
-    # overlay augmented image onto aruco
-    tl = bbox[0][0][0], bbox[0][0][1]
-    tr = bbox[0][1][0], bbox[0][1][1]
-    br = bbox[0][2][0], bbox[0][2][1]
-    bl = bbox[0][3][0], bbox[0][3][1]
-
-    h, w, c = imgAug.shape
-    pts1 = np.array([tl, tr, br, bl])
-    pts2 = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
-    matrix, _ = cv2.findHomography(pts2, pts1)
-    imgOut = cv2.warpPerspective(imgAug, matrix, (img.shape[1], img.shape[0]))
-    cv2.fillConvexPoly(img, pts1.astype(int), (0, 0, 0))
-    imgOut = img + imgOut
-
-    return imgOut
-
-
-def gen_frames():
-    # load image files into dictionary
-    augDics = loadAugImages("static/AugImg")
-
-    try:
-        while True:
-            success, frame = camera.read()  # read the camera frame
-            # detect for aruco markers
-            if not success:
-                break
-            else:
-                arucoFound = findArucoMarkers(frame, draw=True)
-
-            # overlay defined image onto aruco
-            if len(arucoFound[0]) != 0:
-                for bbox, id in zip(arucoFound[0], arucoFound[1]):
-                    if int(id) in augDics.keys():
-                        frame = augmentAruco(
-                            bbox, id, frame, augDics[int(id)], drawID=False)
-
-            # send frame to html
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            # concat frame one by one and show result
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    except KeyboardInterrupt:
-        print("releasing cameras...")
-        camera.release()
-        client.disconnect()
-
-
 # --- Flask templates ---
 @app.route('/main')
 def main():
     return render_template('main.html')
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/loadScript', methods=['GET', 'POST'])
 def loadScript():
@@ -185,6 +74,7 @@ def loadScript():
 
     return render_template('loadScript.html', file_content=file_content)
 
+@app.route('/')
 @app.route('/disclaimer')
 def disclaimer():
     return render_template('disclaimer.html')
@@ -211,3 +101,5 @@ if __name__ == "__main__":
     # testMQTT(client)
     runMQTT(client)
     app.run(host='0.0.0.0', port=8000, debug=False)
+    # with open("Final_Robot_Website/RobotControllerMQTT.py") as file:
+    #     exec(file.read())
